1ac2474059d7d3a738a3b8717b94701380a2489c
Aleksandra <burashnikova@phystech.edu>
2018-05-17 12:25:31 +0300

    batchify by tokens + func for lr as in transformer

--- data.py
+++ data.py
@@ -11,6 +11,76 @@ import const
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+############### Load text by tokens as in MOS ###########################
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = []
+        self.counter = Counter()
+        self.total = 0
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        token_id = self.word2idx[word]
+        self.counter[token_id] += 1
+        self.total += 1
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
+
+
+class Corpus(object):
+    def __init__(self, path):
+        self.dictionary = Dictionary()
+        self.train = self.tokenize(os.path.join(path, 'train.txt'))
+        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
+        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+
+    def tokenize(self, path):
+        """Tokenizes a text file."""
+        assert os.path.exists(path)
+        # Add words to the dictionary
+        with open(path, 'r', encoding='utf-8') as f:
+            tokens = 0
+            for line in f:
+                words = line.split() + ['<eos>']
+                tokens += len(words)
+                for word in words:
+                    self.dictionary.add_word(word)
+
+        # Tokenize file content
+        with open(path, 'r', encoding='utf-8') as f:
+            ids = torch.LongTensor(tokens)
+            token = 0
+            for line in f:
+                words = line.split() + ['<eos>']
+                for word in words:
+                    ids[token] = self.dictionary.word2idx[word]
+                    token += 1
+
+        return ids
+    
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    print(data.size())
+    return data
+
+def get_batch(source, i, seq_len=35, evaluation=False):
+    seq_len = min(seq_len, len(source) - 1 - i)
+    data = Variable(source[i:i+seq_len], volatile=evaluation)
+    # target = Variable(source[i+1:i+1+seq_len].view(-1))
+    target = Variable(source[i+1:i+1+seq_len])
+    return data, target
+
+###############################################
 
 class DataSet:
     def __init__(self, datapath, batch_size=1, build_dict=False, display_freq=0, max_dict=10000, max_len=100, trunc_len=100):
